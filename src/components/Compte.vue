64b2bd137f2d06a57b7d59a44e263e9e837e1085
Smirow <magued1509@gmail.com>
2017-12-17 16:59:02 +0100

    some style

--- src/components/Compte.vue
+++ src/components/Compte.vue
@@ -1,51 +1,42 @@
 <template>
-	<div>
+<div>
 	<div class="row">
     <div class="col-lg-12 col-xl-6">
-				<div class="card-box widget-box-1">
-						<h2 class="font-20">Mes infos</h2>	
-                        <h4 class="font-18"><b>Mon pseudo :</b> {{ pseudo }}	</h4>
-                        <br>
-                        <h4 class="font-18"><b>Mon email :</b> {{ email }}	</h4>
-
-				</div>
-	</div>
-
-	 <div class="col-lg-12 col-xl-6">
-				<div class="card-box widget-box-1">
-						<h2 class="font-20">Ma colloc</h2>	
-                        <h4 class="font-18"><b>Nom :</b> {{ collocName }}	</h4>
-                        <br>
-                        <h4 class="font-18"><b>Membres :</b> </h4>
-
-						<ul style="list-style-type: none;">
-						<li v-for="m in members">
-							<div class="row">								
-									<p class="text-left">{{ m }}</p>								
-							</div>
-						</li>
-					</ul>
-
-
-
-				</div>
-	</div>
-
-	<div class="col-lg-12 col-xl-6">
+			<div class="card-box widget-box-1">
+				<h2 class="font-20">Mes infos</h2>
+				<hr>
+				<h4 class="font-18"><b>Mon pseudo :</b> {{ pseudo }}</h4>
+				<h4 class="font-18"><b>Mon email :</b> {{ email }}</h4>
+			</div>
 				<div class="card-box widget-box-1">
 						<h2 class="font-20">Supprimer mon compte</h2>
 						<div class="col-3">
 							<button class="btn btn-pink btn-block text-uppercase waves-effect waves-light" @click="delAccount()">Supprimer</button>
 						</div>
-
 				</div>
 	</div>
 
+	 <div class="col-lg-12 col-xl-6">
+		<div class="card-box widget-box-1">
+				<h2 class="font-20">Ma colloc</h2>
+				<hr>
+				<h4 class="font-18"><b>Nom :</b> {{ collocName }}	</h4>
+				<br>
+				<h4 class="font-18"><b>Membres :</b> </h4>
 
+				<ul style="list-style-type: none;">
+				<li v-for="m in members">
+					<div class="row">
+							<p class="text-left">{{ m }}</p>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
 
 	</div>
-    	
-	</div>
+
+</div>
 </template>
 
 <script>
@@ -56,7 +47,7 @@ export default {
 	data () {
 		return {
 			pseudo: '',
-        	email: '',
+    	email: '',
 			collocName: '',
 			members: ['cul', 'bite', 'ta mère']
 		};
@@ -70,8 +61,8 @@ export default {
 	},
 	beforeCreate () {
 		this.$http.get(config.url + 'RoomMates/' + auth.getAuthId(), auth.getAuthHeader()).then(response => {
-    		this.pseudo = response.body.username;
-    		this.email = response.body.email;
+			this.pseudo = response.body.username;
+			this.email = response.body.email;
 			this.collocName = response.body.colloc;
 		});
 	}
